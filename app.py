56fa7aa310bd9147b068ee2a2b7cc9ec03ff4f11
zolloalf <>
2024-10-05 22:38:41 +0500

    added routes for folder.html and for download handling

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 
 app = Flask(__name__)
 
@@ -6,6 +6,16 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('folder')
+def folder():
+    return render_template('folder.html')
+
+
+@app.route('download')
+def download():
+    return send_file()
 
 
 if __name__ == '__main__':
