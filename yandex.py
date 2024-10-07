de6015a80ef7c1dc79fd31e4cf3dcb58ac7f66c3
zolloalf <>
2024-10-07 13:33:41 +0500

    added type annotations and functions for get download links

--- yandex.py
+++ yandex.py
@@ -1,20 +1,44 @@
-from flask import session
+from flask import request, session
 import requests
 
 info_url: str = 'https://cloud-api.yandex.net/v1/disk/public/resources'
+download_url: str = 'https://cloud-api.yandex.net/v1/disk/public/resources/download'
 
 
-def get_files_list(public_key, path):
-    headers = {
+def get_files_list(public_key: str, path: str):
+    headers: dict = {
         'Accept': 'application/json',
         'Authorization': f'OAuth {session.get('yandex_token')}'
     }
-    params = {
+    params: dict = {
         'public_key': public_key,
         'path': path
     }
-    response = requests.get(url=info_url, headers=headers, params=params)
+    response: requests.Response = requests.get(
+        url=info_url, headers=headers, params=params)
     if response.status_code != 200:
         return []
-    files_list = response.json().get('_embedded').get('items')
+    files_list: list = response.json().get('_embedded').get('items')
     return files_list
+
+
+def get_download_link(path: str):
+    public_key: str = request.form.get('public_key')
+    response: requests.Response = requests.get(url=download_url, params={
+        'public_key': public_key,
+        'path': path
+    })
+    if response.status_code != 200:
+        return ''
+    return response.json()['href']
+
+
+def get_download_links(paths: str):
+    file_urls: list = []
+
+    for path in paths:
+        link: str = get_download_link(path)
+        if not link:
+            return []
+        file_urls.append(link)
+    return file_urls
