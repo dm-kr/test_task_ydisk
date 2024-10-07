956eaf10a0cb33d05678f08d0f956f20d830d503
zolloalf <>
2024-10-07 12:52:35 +0500

    added authorization functionality

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
-from flask import Flask, render_template, send_file, Response
+from flask import Flask, Response, redirect, render_template, send_file, url_for, session
+from authlib.integrations.flask_client import OAuth
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app: Flask = Flask(__name__)
+
+app.secret_key = os.getenv('SECRET_KEY')
+
+oauth: OAuth = OAuth(app)
+
+oauth.register(
+    name='yandex',
+    client_id=os.getenv('CLIENT_ID'),
+    client_secret=os.getenv('CLIENT_SECRET'),
+    authorize_url='https://oauth.yandex.ru/authorize',
+    access_token_url='https://oauth.yandex.ru/token',
+    userinfo_endpoint='https://login.yandex.ru/info',
+    client_kwargs={'scope': 'login:email login:info'},
+)
 
 info_url: str = 'https://cloud-api.yandex.net/v1/disk/public/resources'
 download_url: str = 'https://cloud-api.yandex.net/v1/disk/public/resources/download'
@@ -11,12 +30,34 @@ def index() -> str:
     return render_template('index.html')
 
 
-@app.route('folder')
+@app.route('/login')
+def login() -> None:
+    redirect_uri: str = url_for('authorize', _external=True)
+    return oauth.yandex.authorize_redirect(redirect_uri)
+
+
+@app.route('/authorize')
+def authorize() -> Response:
+    token: str = oauth.yandex.authorize_access_token()
+    session['yandex_token'] = token
+    user_info: dict = oauth.yandex.get('https://login.yandex.ru/info').json()
+    session['user_info'] = user_info
+    return redirect(url_for('index'))
+
+
+@app.route('/logout')
+def logout() -> Response:
+    session.pop('yandex_token', None)
+    session.pop('user_info', None)
+    return redirect(url_for('index'))
+
+
+@app.route('/folder')
 def folder() -> str:
     return render_template('folder.html')
 
 
-@app.route('download')
+@app.route('/download')
 def download() -> Response:
     return send_file()
 
