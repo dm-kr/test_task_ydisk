e2e38dd678e732219a9f8b7644097f0b4791f461
zolloalf <>
2024-10-07 13:33:02 +0500

    added type annotations

--- decorators.py
+++ decorators.py
@@ -1,10 +1,11 @@
 from flask import render_template, session
 from functools import wraps
+from typing import Callable, Any
 
 
-def need_login(f):
+def need_login(f: Callable[..., Any]) -> Callable[..., Any]:
     @wraps(f)
-    def wrapper(*args, **kwargs):
+    def wrapper(*args: Any, **kwargs: Any) -> Any:
         if 'yandex_token' not in session:
             return render_template('login.html')
         return f(*args, **kwargs)
