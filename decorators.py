7a9822e7bd85b54ea6e556e764d4f5d8dab0a09a
zolloalf <>
2024-10-07 13:38:02 +0500

    added cache decorator

--- decorators.py
+++ decorators.py
@@ -1,6 +1,7 @@
 from flask import render_template, session
 from functools import wraps
 from typing import Callable, Any
+import threading
 
 
 def need_login(f: Callable[..., Any]) -> Callable[..., Any]:
@@ -9,5 +10,27 @@ def need_login(f: Callable[..., Any]) -> Callable[..., Any]:
         if 'yandex_token' not in session:
             return render_template('login.html')
         return f(*args, **kwargs)
+
+    return wrapper
+
+
+def cache(f: Callable[..., Any], time=60) -> Callable[..., Any]:
+    cache: dict = {}
+
+    @wraps(f)
+    def wrapper(*args: str, **kwargs: Any) -> Any:
+        key: str = '-'.join([arg for arg in args if arg])
+        if key in cache:
+            return cache[key]
+        data: list = f(*args, **kwargs)
+        cache[key] = data
+
+        def delete_cache():
+            cache.pop(key)
+
+        timer: threading.Timer = threading.Timer(time, delete_cache)
+        timer.start()
+
+        return data
 
     return wrapper
